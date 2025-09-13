794b233054c51b8fb95df3b606e4296cac38b4b1
4nddrs <joseandres190503@gmail.com>
2025-09-13 02:10:53 -0400

    Add icons for devices

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -5,6 +5,7 @@ import { googleFontHref, googleFontSubsetHref } from "../util/theme"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { unescapeHTML } from "../util/escape"
 import { CustomOgImagesEmitterName } from "../plugins/emitters/ogImage"
+
 export default (() => {
   const Head: QuartzComponent = ({
     cfg,
@@ -25,9 +26,18 @@ export default (() => {
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
-    const iconPath = joinSegments(baseDir, "static/icon.png")
 
-    // Url of current page
+    // Ruta de los iconos
+    const iconsBase = "/static/icons"
+    const iconPath = joinSegments(iconsBase, "favicon-96x96.png")
+    const appleTouchIcon = joinSegments(iconsBase, "apple-touch-icon.png")
+    const manifestPath = joinSegments(iconsBase, "site.webmanifest")
+    const android192 = joinSegments(iconsBase, "web-app-manifest-192x192.png")
+    const android512 = joinSegments(iconsBase, "web-app-manifest-512x512.png")
+    const faviconSvg = joinSegments(iconsBase, "favicon.svg")
+    const faviconIco = joinSegments(iconsBase, "favicon.ico")
+
+    // URL para Open Graph / social
     const socialUrl =
       fileData.slug === "404" ? url.toString() : joinSegments(url.toString(), fileData.slug!)
 
@@ -53,6 +63,7 @@ export default (() => {
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        {/* Open Graph / Twitter */}
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
@@ -61,7 +72,6 @@ export default (() => {
         <meta name="twitter:description" content={description} />
         <meta property="og:description" content={description} />
         <meta property="og:image:alt" content={description} />
-
         {!usesCustomOgImage && (
           <>
             <meta property="og:image" content={ogImageDefaultPath} />
@@ -73,7 +83,6 @@ export default (() => {
             />
           </>
         )}
-
         {cfg.baseUrl && (
           <>
             <meta property="twitter:domain" content={cfg.baseUrl}></meta>
@@ -82,10 +91,26 @@ export default (() => {
           </>
         )}
 
-        <link rel="icon" href={iconPath} />
+        {/* Favicon y Apple Touch Icon */}
+        <link rel="icon" type="image/png" sizes="96x96" href={iconPath} />
+        <link rel="icon" type="image/svg+xml" href={faviconSvg} />
+        <link rel="shortcut icon" href={faviconIco} />
+        <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+
+        {/* Android / PWA */}
+        <link rel="icon" type="image/png" sizes="192x192" href={android192} />
+        <link rel="icon" type="image/png" sizes="512x512" href={android512} />
+        <link rel="manifest" href={manifestPath} />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Microsoft Tiles */}
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content={iconPath} />
+
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
+        {/* CSS / JS / additional head */}
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
